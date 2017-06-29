6b8edb49542cd46574fd037f30e2dfb78ed04a7a
Maria Fedosova <mariafedosova@imac-maria.local>
2017-06-29 17:23:26 +0300

    Initial Commit

---
+++ My_02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My_02
+//
+//  Created by Maria Fedosova on 29.06.17.
+//  Copyright © 2017 Konstantin Freidlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
